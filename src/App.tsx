deb10f2eb715372ae07cdcd8d6a0ba89c3c304b3
Elias Júnior <me@eliasjr.dev>
2023-06-21 12:36:03 -0300

    feat: setup landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Button, ChakraProvider, Image, Input, InputGroup, InputLeftAddon, InputRightElement, Text } from '@chakra-ui/react';
+import logoPagBank from './assets/logo-pagbank.svg'
+import { useMemo, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+const httpsRegex = /^https?:\/\//
 
+const validateUrl = (url: string) => urlRegex.test(url)
+
+const App: React.FC = () => {
+  const [url, setUrl] = useState<string>('')
+  const isUrl = useMemo(() => validateUrl(`https://${url}`), [url])
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const redirectUrl = `https://${url}/wp-admin/plugin-install.php?s=pagbank-woocommerce&tab=search&type=term`
+
+    if(!validateUrl(redirectUrl)) {
+      alert('URL inválida')
+
+      return
+    }
+
+    window.location.href = `${redirectUrl}`
+  }
+
+  const onChangeUrl = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target
+    const clearedValue = value.trim()
+    
+    if(httpsRegex.test(clearedValue)) {
+      event.target.value = clearedValue.replace(httpsRegex, '')
+    }
+
+    event.target.value = event.target.value.replace(/\s/g, '')
+
+    setUrl(event.target.value)
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ChakraProvider>
+      <Box h="100vh" marginX="auto" display="flex" flexDirection="column" alignItems="center" justifyContent="center" background="linear-gradient(81.51deg,#ffe72d 2.89%,#fff4a0 87.51%,#fff 104.08%)">
+        <Image src={logoPagBank} alt="PagBank" w={96} marginX="auto"/>
+
+        <Text mt={4}>Preencha o campo abaixo com a URL da sua loja e iremos te redirecionar para instalar o nosso plugin:</Text>
+
+        <Box as="form" w="100%" maxW="500px" display="flex" alignItems="center" flexDir="column" onSubmit={onSubmit}>
+          <InputGroup mt={12} size="lg" borderColor="#1bb99a">
+            <InputLeftAddon children="https://" backgroundColor="#1bb99a" color="#ffffff" />
+            <Input placeholder='seu-site.com.br' pr={28} onChange={onChangeUrl} color="#1bb99a" backgroundColor="#ffffff" />
+            <InputRightElement w={24} >
+              <Button type="submit" w="100%" mr={1} isDisabled={!isUrl} backgroundColor="#1bb99a" color="#ffffff" _hover={{backgroundColor: '#1a947b'}}>
+                Instalar
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </Box>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pagbank-woocommerce/'
+  base: '/pagbank-woocommerce/',
+  build: {
+    outDir: 'docs'
+  }
 })
