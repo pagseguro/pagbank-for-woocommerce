add69b047b0db26f2524fa62ddca590871fd1f8b
Elias Júnior <me@eliasjr.dev>
2023-06-27 12:52:41 -0300

    feat: add logs to webhooks

--- src/core/Presentation/WebhookHandler.php
+++ src/core/Presentation/WebhookHandler.php
@@ -7,6 +7,8 @@
 
 namespace PagBank_WooCommerce\Presentation;
 
+use WC_Logger;
+
 /**
  * Class WebhookHandler.
  */
@@ -20,10 +22,25 @@ class WebhookHandler {
 	private static $instance = null;
 
 	/**
+	 * Logger.
+	 *
+	 * @var WC_Logger
+	 */
+	private $logger;
+
+	/**
 	 * Initialize webhook handler.
 	 */
 	public function __construct() {
 		add_action( 'woocommerce_api_pagbank_woocommerce_handler', array( $this, 'handle' ) );
+		add_action( 'init', array( $this, 'init_logs' ) );
+	}
+
+	/**
+	 * Initialize logs.
+	 */
+	public function init_logs() {
+		$this->logger = new WC_Logger();
 	}
 
 	/**
@@ -47,13 +64,30 @@ class WebhookHandler {
 	}
 
 	/**
+	 * Log.
+	 *
+	 * @param string $message Message.
+	 */
+	private function log( string $message ): void {
+		if ( ! $this->logger ) {
+			return;
+		}
+
+		$this->logger->add( 'pagbank_webhook', $message );
+	}
+
+	/**
 	 * Handle webhook.
 	 */
 	public function handle() {
-		$payload  = json_decode( file_get_contents( 'php://input' ), true );
-		$order_id = $payload['reference_id'];
+		$input    = file_get_contents( 'php://input' );
+		$payload  = json_decode( $input, true );
+		$order_id = isset( $payload['reference_id'] ) ? $payload['reference_id'] : null;
+
+		$this->log( 'Webhook received: ' . $input );
 
 		if ( empty( $order_id ) ) {
+			$this->log( 'Webhook validation failed: order_id is empty' );
 			return wp_send_json_error(
 				array(
 					'message' => __( 'Pedido não encontrado.', 'pagbank-woocommerce' ),
@@ -65,6 +99,7 @@ class WebhookHandler {
 		$order = wc_get_order( $order_id );
 
 		if ( ! $order ) {
+			$this->log( 'Webhook validation failed: order not found' );
 			return wp_send_json_error(
 				array(
 					'message' => __( 'Pedido não encontrado.', 'pagbank-woocommerce' ),
@@ -80,6 +115,7 @@ class WebhookHandler {
 			$signature = $charge['metadata']['signature'];
 
 			if ( ! $signature ) {
+				$this->log( 'Webhook validation failed: missing signature for order id ' . $order_id );
 				return wp_send_json_error(
 					array(
 						'message' => __( 'Assinatura não encontrada.', 'pagbank-woocommerce' ),
@@ -91,6 +127,7 @@ class WebhookHandler {
 			$is_valid_signature = validate_order_id_signature( $order->get_id(), $signature );
 
 			if ( ! $is_valid_signature ) {
+				$this->log( 'Webhook validation failed: invalid signature for order id ' . $order_id . ' (' . $signature . ')' );
 				return wp_send_json_error(
 					array(
 						'message' => __( 'Assinatura inválida.', 'pagbank-woocommerce' ),
@@ -107,6 +144,8 @@ class WebhookHandler {
 		} elseif ( $charge['status'] === 'PAID' ) {
 			$order->payment_complete( $charge['id'] );
 		}
+
+		$this->log( 'Webhook processed successfully' );
 
 		wp_send_json_success(
 			array(
